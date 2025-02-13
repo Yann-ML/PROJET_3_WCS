496b10a92503ccffbe2990ad3b2657c7157f7984
Yann-ML <macleod.yann@gmail.com>
2025-02-13 13:31:48 +0100

    Ajout liste tracks 5000 albums sur 6000 + modif SpotiDash

--- SpotiDash.py
+++ SpotiDash.py
@@ -5,12 +5,26 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc 
+import google.generativeai as genai  #IA
+from dotenv import load_dotenv  # IA
+import os  # IA
 import plotly.express as px
 import pandas as pd
 
 # Création de l'application
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+
+
+################################## CHARGEMENT VARIABLE ENVIRONNEMENT ##################################
+
+load_dotenv()
+
+################################## CONNEXION GOOGLE CHATBOT ##################################
+
+genai.configure(api_key= os.getenv("GOOGLE_API_KEY"))
+
 ################################## LECTURE FICHIER ##################################
+
 
 # Lien vers le fichier
 link = 'https://raw.githubusercontent.com/Yann-ML/PROJET_3_WCS/main/API_SPOTIFY/df_spotify_1754.zip'
@@ -23,12 +37,9 @@ list_artiste = sorted(df['artist_name'].unique().tolist())
 # nettoyage de la colonne genre car vue comme du str
 df['genres_artist_clean'] = df['genres_artist'].apply(lambda x: x.replace('[', '').replace(']', '').replace("'", ""))
 
-# artiste recherché
-search_artist = ''
-
-# 
 
 ################################## STYLE DES AFFICHAGES ##################################
+
 
 button_style = {
     'padding': '10px 20px',
@@ -43,16 +54,9 @@ button_style = {
     'display': 'inline-block'
 }
 
+
 ################################## APPLICATION ##################################
 
-fig = px.bar(data_frame=df[['album_name', 'artist_name', 'total_tracks']][df['artist_name'] == ''],
-    x='artist_name'
-    )
-
-fig2 = px.bar(data_frame=df[['album_name', 'artist_name', 'total_tracks']][df['artist_name'] == ''],
-    x='album_name',
-    y='total_tracks'
-    )
 
 # Titres et textes
 app.layout = html.Div([
@@ -159,13 +163,21 @@ app.layout = html.Div([
             ], style={'display': 'flex', 'flexDirection': 'column'})
              
     # affichage bloc album+titre en ligne
-    ], style={'display': 'flex', 'flexDirection': 'row'})
+    ], style={'display': 'flex', 'flexDirection': 'row'}),
+    
+    # affichage biographie par le chat
+    html.Div([
+        html.Label(
+                id='bio_artist'
+                )       
+            ])
 ])
 
 
 ################################## APPEL DES COMPOSANTS ##################################
 
 
+# callback d'appel des images artistes, pochette albums, puis biographie par le chat
 @app.callback(
     [Output(component_id="img_artist", component_property='src'),
      Output(component_id="pop_artist", component_property='children'),
@@ -174,21 +186,56 @@ app.layout = html.Div([
      Output(component_id="img_album2", component_property='src'),
      Output(component_id="title_album2", component_property='children'),
      Output(component_id="img_album3", component_property='src'),
-    Output(component_id="title_album3", component_property='children')],
-    
+     Output(component_id="title_album3", component_property='children'),
+     Output(component_id='bio_artist', component_property='children')],
+        
     Input(component_id="input_search", component_property="value")
 )
 
+# fonction d'appel des éléments à récupérer dans le DF
 def update_album_image(value):
     if value:
-        # On récupère l'album le plus populaire de l'artiste sélectionné
+        
+        # récup 3 albums les plus populaires de l'artiste sélectionné
         album_info = df[['album_name', 'popularity_album', 'popularity_artist', 'image_artist', 'artist_name', 'url_image_album']][df['artist_name'] == value].drop_duplicates().sort_values(by='popularity_album', ascending=False).iloc[0:3]
-        # track_info = 
-        return album_info['image_artist'].iloc[0], f"Popularité : {album_info['popularity_artist'].iloc[0]}",\
-                album_info['url_image_album'].iloc[0], album_info['album_name'].iloc[0],\
-                album_info['url_image_album'].iloc[1], album_info['album_name'].iloc[1],\
-                album_info['url_image_album'].iloc[2], album_info['album_name'].iloc[2]
-    return ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '  # Si aucun artiste n'est sélectionné, ne pas afficher d'image
+        
+        # gestion si moins de 3 albums
+        album1 = album_info.iloc[0] if len(album_info) > 0 else None
+        album2 = album_info.iloc[1] if len(album_info) > 1 else None
+        album3 = album_info.iloc[2] if len(album_info) > 2 else None
+
+        # récup pochette et titres albums
+        img_album1 = album1['url_image_album'] if album1 is not None else ' '
+        title_album1 = album1['album_name'] if album1 is not None else ' '
+        
+        img_album2 = album2['url_image_album'] if album2 is not None else ' '
+        title_album2 = album2['album_name'] if album2 is not None else ' '
+        
+        img_album3 = album3['url_image_album'] if album3 is not None else ' '
+        title_album3 = album3['album_name'] if album3 is not None else ' '
+
+        # récup image et popularité artiste
+        artist_info = df[df['artist_name'] == value].iloc[0]
+        img_artist = artist_info['image_artist']
+        pop_artist = f"Popularité : {artist_info['popularity_artist']}"
+        
+        # génération du modèle d'IAG
+        model = genai.GenerativeModel('gemini-1.5-flash')
+        
+        # définition du prompt
+        prompt = f"Peux tu m'afficher le maximum d'informations sur {value} dans un style biographie d'artiste ? Je veux un bloc de 10 lignes maximum, je ne veux pas de MarkDown dans le texte. soit si tu veux afficher un mot en gras mets le en gras directement"
+        
+        # réponse du prompt et génération
+        reponse = model.generate_content(prompt)
+        texte = html.P(reponse.text) 
+        
+        return img_artist, pop_artist,\
+                img_album1, title_album1,\
+                img_album2, title_album2,\
+                img_album3, title_album3,\
+                texte
+        
+    return ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '  # Si aucun artiste n'est sélectionné, ne pas afficher d'image
 
 if __name__ == '__main__':
     app.run_server(debug=True)
